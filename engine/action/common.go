cd0d2dd00f53aec03c6b4a700bac4034f73f6a23
heyitong <heyitong@iie.ac.cn>
2021-07-06 18:43:34 +0800

    ...

--- engine/action/common.go
+++ engine/action/common.go
@@ -12,6 +12,7 @@ func New(actionInfo *models.ActionInfo, info *models.WorkflowNodeInfo) types.Act
 	case models.ActionType_BuildInAction:
 	case models.ActionType_CodeAction:
 	case models.ActionType_EngineAction:
+		return NewRemoteServiceAction(actionInfo, info)
 	case models.ActionType_WorkflowAction:
 	default:
 
