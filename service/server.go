f8ebb06ecdb8b3d59f8fc721371ee2b2dff39caf
heyitong <heyitong@iie.ac.cn>
2022-04-14 17:26:32 +0800

    添加执行器模板

--- service/server.go
+++ service/server.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/golang/protobuf/proto"
+	models2 "github.com/qhzhyt/tiops-go-sdk/common/models"
 	"google.golang.org/grpc"
 	"log"
 	"math"
@@ -200,6 +201,7 @@ func newActionServer() *actionServer {
 		apiClient:        tiopsApiClient,
 		Logger:           remoteLogger,
 		actionContextMap: map[string]*actionTypes.ActionContext{},
+		actionInfoMap: map[string]*models2.ActionInfo{},
 		//actionNodeOptionsMap: map[string]ActionOptions{},
 		actionNodeContextMap: map[string]*actionTypes.ActionNodeContext{},
 		requiredResourcesMap: map[int]*models.WorkflowResources{},
