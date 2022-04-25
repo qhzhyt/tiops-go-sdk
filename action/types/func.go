7d6679b51e5f10d133a10d22f85eaef5b5ae72aa
heyitong <heyitong@iie.ac.cn>
2022-04-25 10:36:02 +0800

    添加BatchProcessFunction

--- action/types/func.go
+++ action/types/func.go
@@ -7,6 +7,11 @@ type ExecutorFunc func(ctx *ActionNodeContext) (ItemProcessFunc, error)
 //type ActionFunction func(requestContext *PieceRequestContext) ActionDataItem
 
 type PieceProcessFunction func(requestContext *PieceRequestContext) ActionDataItem
+type BatchProcessFunction func(requestContext *BatchRequestContext) ActionDataBatch
+
+type PieceFunctionAction PieceProcessFunction
+type BatchFunctionAction BatchProcessFunction
+
 //func (af ActionFunction) Init(ctx *InitContext) {
 //
 //}
@@ -15,13 +20,12 @@ type PieceProcessFunction func(requestContext *PieceRequestContext) ActionDataIt
 //	return nil
 //}
 
-func (pf PieceProcessFunction) Call(ctx *PieceRequestContext) ActionDataItem {
+func (pf PieceFunctionAction) Call(ctx *PieceRequestContext) ActionDataItem {
 	return pf(ctx)
 }
 
-type BatchProcessFunction func(requestContext *BatchRequestContext) ActionDataBatch
 
-func (bf BatchProcessFunction) CallBatch(ctx *BatchRequestContext) ActionDataBatch {
+func (bf BatchFunctionAction) CallBatch(ctx *BatchRequestContext) ActionDataBatch {
 	return bf(ctx)
 }
 //func (af ActionFunction) CallBatch(ctx *BatchRequestContext) ActionDataBatch {
--- service/ctl.go
+++ service/ctl.go
@@ -23,10 +23,16 @@ func (a *actionServerCtl) Register(name string, action interface{}) *actionServe
 		a.actionServer.RegisterEngine(name, action0)
 	case actionTypes.PieceProcessFunction:
 		logger.Info("Register PieceProcessFunction " + name)
-		a.actionServer.RegisterAction(name, action0)
+		a.actionServer.RegisterAction(name, actionTypes.PieceFunctionAction(action0))
 	case actionTypes.BatchProcessFunction:
 		logger.Info("Register BatchProcessFunction " + name)
-		a.actionServer.RegisterAction(name, action0)
+		a.actionServer.RegisterAction(name, actionTypes.BatchFunctionAction(action0))
+	case func(ctx *actionTypes.PieceRequestContext) actionTypes.ActionDataItem:
+		logger.Info("Register func(ctx *actionTypes.PieceRequestContext) actionTypes.ActionDataItem " + name)
+		a.actionServer.RegisterAction(name, actionTypes.PieceFunctionAction(action0))
+	case func(ctx *actionTypes.BatchRequestContext) actionTypes.ActionDataBatch:
+		logger.Info("Register func(ctx *actionTypes.BatchRequestContext) actionTypes.ActionDataBatch " + name)
+		a.actionServer.RegisterAction(name, actionTypes.BatchFunctionAction(action0))
 	case actionTypes.Action:
 		logger.Info("Register Action " + name)
 		a.actionServer.RegisterAction(name, action0)
